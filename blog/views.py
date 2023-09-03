c1f96cc86fa1c10d212eb39546b95423057196ff
Mohammad Jayeed <thisisjayeed@gmail.com>
2023-09-03 20:25:35 +0600

    minor changes on frontend

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Blog
 # Create your views here.
 def home(request):
     categories = Category.objects.all()
-    return render(request,'home-blogs.html',{'categories': categories})
+    blog = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    blog_normal = Blog.objects.filter(is_featured=False, status='Published').order_by('updated_at')
+    
+    return render(request,'home-blogs.html',{'categories': categories, 'blogs':blog, 'blog_posts':blog_normal})
