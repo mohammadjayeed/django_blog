e5838c1a5682b754b110dae56d82f0addd9563fd
Mohammad Jayeed <thisisjayeed@gmail.com>
2023-09-07 17:11:18 +0600

    minor updates on post by category

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Category, Blog
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
@@ -11,10 +12,15 @@ def home(request):
     return render(request,'home-blogs.html',{'categories': categories, 'blogs':blog, 'blog_posts':blog_normal})
 
 def posts_by_category(request, category_id):
+    
     posts = Blog.objects.filter(status='Published', category = category_id)
-
-
-    context = {'posts':posts}
+    # try:
+    #     category = Category.objects.get(pk= category_id)
+    # except:
+    #     return redirect('home')
+    category= get_object_or_404(Category,pk =category_id)
+    context = {'posts':posts,
+               'category':category}
 
     return render(request, 'blog/posts_by_category.html', context)
 
